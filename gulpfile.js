824adeb9cca588c43af8127670ec5522d59d964d
Michael Staehling <Michael.staehling@gmail.com>
2016-10-20 14:02:32 -0400

    added wiredep and gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var wiredep = require('wiredep').stream;
+
+// inject bower components....MAY HAVE TO FIX SRC PATHS
+gulp.task('wiredep', () => {
+  gulp.src('app/styles/*.scss')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)+/
+    }))
+    .pipe(gulp.dest('app/styles'));
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      exclude: ['bootstrap-sass'],
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
+});
 
 gulp.task('default', function() {
-  
+
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
