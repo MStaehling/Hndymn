f07a13718be8163f637fbecef412c28fc5020590
Michael Staehling <Michael.staehling@gmail.com>
2016-10-20 16:06:24 -0400

    updated gulp file to configure sass and added bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,62 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var wiredep = require('wiredep').stream;
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify-css');
+var merge = require('merge-stream');
+const autoprefixer = require('gulp-autoprefixer');
+
+const $ = gulpLoadPlugins();
 
 // inject bower components....MAY HAVE TO FIX SRC PATHS
 gulp.task('wiredep', () => {
-  gulp.src('app/styles/*.scss')
-    .pipe(wiredep({
-      ignorePath: /^(\.\.\/)+/
-    }))
-    .pipe(gulp.dest('app/styles'));
-  gulp.src('app/*.html')
-    .pipe(wiredep({
-      exclude: ['bootstrap-sass'],
-      ignorePath: /^(\.\.\/)*\.\./
-    }))
-    .pipe(gulp.dest('app'));
+    gulp.src('app/styles/*.scss')
+        .pipe(wiredep({
+            ignorePath: /^(\.\.\/)+/
+        }))
+        .pipe(gulp.dest('app/styles'));
+    gulp.src('app/html/*.html')
+        .pipe(wiredep({
+            exclude: ['bootstrap-sass'],
+            ignorePath: /^(\.\.\/)*\.\./
+        }))
+        .pipe(gulp.dest('app'));
 });
 
 gulp.task('default', function() {
 
 });
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+var scssStream = gulp.src(['./app/styles/*.scss'])
+    .pipe(sass())
+    .pipe(concat('scss-files.scss'));
+gulp.task('styles', () => {
+    return gulp.src('./app/styles/*.scss')
+        .pipe(concat('main.scss'))
+        .pipe($.plumber())
+        .pipe($.sourcemaps.init())
+        .pipe($.sass.sync({
+            outputStyle: 'expanded',
+            precision: 10,
+            includePaths: ['.']
+        }).on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: ['last 1 version']
+        }))
+        .pipe($.sourcemaps.write())
+        .pipe(minify())
+        .pipe(gulp.dest('.tmp/'))
+        //.pipe(reload({stream: true}));
+});
+gulp.watch('app/styles/*.scss', ['styles']);
+gulp.task('sass', function() {
+    return gulp.src('./app/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/main.css'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('sass:watch', function() {
+    gulp.watch('./app/**/*.scss', ['sass']);
 });
