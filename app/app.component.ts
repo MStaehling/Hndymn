0ed8b314c1d31c600aa8762f523aa51f500881eb
Michael Staehling <Michael.staehling@gmail.com>
2016-10-28 14:58:46 -0400

    minor adjustments

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
-  selector: 'my-app',
-  templateUrl: 'app/html/nav.html'
+    selector: 'my-app',
+    templateUrl: 'app/html/nav.html'
 })
 export class AppComponent { }
