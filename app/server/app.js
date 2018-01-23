b2fa7d4bc855989bcc2698169133fc4400c5a755
Michael Staehling <Michael.staehling@gmail.com>
2018-01-22 20:03:13 -0500

    update packages

--- app/server/app.js
+++ app/server/app.js
@@ -5,6 +5,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
@@ -17,13 +23,70 @@ if (config.seedDB) {
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 require('./config/express')(app);
 require('./routes')(app);
 
-// Start server
-server.listen(config.port, config.ip, function() {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  // var server = app.listen(process.env.PORT || 8080, function () {
+  //   var port = server.address().port;
+  //   console.log("App now running on port", port);
+  // });
+
+  // Start server
+  server.listen(config.port, config.ip, function() {
+    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  });
+});
+
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/stops", function(req, res) {
+});
+
+app.post("/stops", function(req, res) {
+});
+
+/*  "/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/stops/:id", function(req, res) {
+});
+
+app.put("/stops/:id", function(req, res) {
+});
+
+app.delete("/stops/:id", function(req, res) {
 });
 
 // Expose app
